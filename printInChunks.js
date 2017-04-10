96bdfe80db8c1d569b4fec11843cefd649242f05
Jinny Huang <jinny921@gmail.com>
2017-04-10 18:42:25 +0000

    requested a big chunk of data

---
+++ printInChunks.js
@@ -0,0 +1,26 @@
+const https = require('https');
+
+function getAndPrintHTMLChunks () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  /* Add your code here */
+  https.get(requestOptions, function(response) {
+
+    response.setEncoding('utf8');
+
+    response.on('data', function(data) {
+      console.log(`chunk of data: ${data}`);
+    });
+
+    response.on('end', function() {
+      console.log(`response stream completed`);
+    });
+  })
+
+};
+
+getAndPrintHTMLChunks();
