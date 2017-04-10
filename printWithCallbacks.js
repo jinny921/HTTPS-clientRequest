8738c5fd014d4aedddc44ada921e10c3335e0ef7
Jinny Huang <jinny921@gmail.com>
2017-04-10 20:20:34 +0000

    added callback function to the client request

---
+++ printWithCallbacks.js
@@ -0,0 +1,31 @@
+const https = require('https');
+
+function getHTML (options, callback) {
+
+  /* Add your code here */
+  https.get(options, function(response) {
+
+    response.setEncoding('utf8');
+
+    let responseString = '';
+
+    response.on('data', (data) => {
+      responseString += data;
+    });
+
+    response.on('end', function() {
+      callback(responseString);
+    });
+  });
+};
+
+function printHTML (html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+getHTML(requestOptions, printHTML);
